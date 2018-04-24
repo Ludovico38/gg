ce2f225536a0394aba356fd5c40eb5cf5c289061
DaFinishGames <forIT@192.168.0.27>
2018-04-24 13:00:26 +0200

    +Add FornitoreDTO+Servlet

---
+++ src/main/java/org/forit/magazzino/servlet/FornitoriServlet.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.forit.magazzino.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author forIT
+ */
+public class FornitoriServlet extends HttpServlet {
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        try(PrintWriter out=resp.getWriter()){
+            out.println("<!DOCTYPE html>");
+        }
+    }
+
+}
